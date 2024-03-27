1498995036aa1084e08174d907adf6f9e5bde594
Vladyslav Vladinov <vladinov.dev@gmail.com>
2024-03-27 12:16:06 +0100

    feat(widgets): added adaptive to footer

--- src/widgets/footer/ui/contact.tsx
+++ src/widgets/footer/ui/contact.tsx
@@ -2,9 +2,9 @@ import { Contacts } from '~/features/contacts';
 
 export const Contact = () => {
   return (
-    <section className="text-center">
+    <section className="text-center md:text-right">
       <h3 className="mb-8 text-2xl/7 font-medium">Contact Us</h3>
-      <Contacts />
+      <Contacts className="md:flex md:flex-col md:items-end" />
     </section>
   );
 };
