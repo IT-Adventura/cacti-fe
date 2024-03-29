f7ea4dc2545d4f50d511b554de6234fbc32420e5
Vladyslav Vladinov <vladinov.dev@gmail.com>
2024-03-29 22:16:18 +0100

    fix(assets): moved images into assets folder

---
+++ src/shared/ui/assets/images/hero/index.ts
@@ -0,0 +1,5 @@
+export { default as w376 } from './hero-bg-376.jpg';
+export { default as w768 } from './hero-bg-768.jpg';
+export { default as w1024 } from './hero-bg-1024.jpg';
+export { default as w1280 } from './hero-bg-1280.jpg';
+export { default as w1440 } from './hero-bg-1440.jpg';
